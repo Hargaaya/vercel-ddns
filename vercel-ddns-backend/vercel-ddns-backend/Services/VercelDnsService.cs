575c370dffb2675e28b9c8eaae77ff304471f78f
Hargaaya <hargaaya10@gmail.com>
2023-01-21 18:45:42 +0100

    implement method GetRecords

--- vercel-ddns-backend/vercel-ddns-backend/Services/VercelDnsService.cs
+++ vercel-ddns-backend/vercel-ddns-backend/Services/VercelDnsService.cs
@@ -1,21 +1,38 @@
-﻿using vercel_ddns_backend.Interfaces.Services;
+﻿using RestSharp;
+using vercel_ddns_backend.Interfaces.Services;
 using vercel_ddns_backend.Models;
 
 namespace vercel_ddns_backend.Services;
 
 public class VercelDnsService : IDnsService
 {
-    public Task<IEnumerable<DnsRecord>?> GetRecords(string domainName)
+    private const string VercelUrl = "https://api.vercel.com/v4/domains";
+    private readonly RestClient _client;
+
+    public VercelDnsService(IConfiguration config)
+    {
+        var apiKey = config.GetValue<string>("DnsApiKey");
+        _client = new RestClient(VercelUrl)
+            .AddDefaultHeader(KnownHeaders.Authorization, $"Bearer {apiKey}");
+    }
+    public async Task<IEnumerable<DnsRecord>?> GetRecords(string domain)
+    {
+        var resp = await _client.GetJsonAsync<VercelGetResponse>($"/{domain}/records");
+        
+        return resp?.records;
+    }
+
+    public Task<DnsRecord?> CreateRecord(string domain, DnsRecord record)
     {
         throw new NotImplementedException();
     }
 
-    public Task<DnsRecord?> UpdateRecord(string domainName, DnsRecord record)
+    public Task<DnsRecord?> UpdateRecord(string domain, DnsRecord record)
     {
         throw new NotImplementedException();
     }
 
-    public Task<bool> DeleteRecord(string domainName, string dnsName)
+    public Task<bool> DeleteRecord(string domain, string dnsName)
     {
         throw new NotImplementedException();
     }
