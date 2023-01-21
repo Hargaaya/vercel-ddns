19213d8fdbfbaa8d11ca01799df6dde4a6a41184
Hargaaya <hargaaya10@gmail.com>
2023-01-21 18:41:30 +0100

    add get response model

---
+++ vercel-ddns-backend/vercel-ddns-backend/Models/VercelGetResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace vercel_ddns_backend.Models;
+
+public record VercelGetResponse
+{
+    public DnsRecord[] records { get; set; }
+    public Pagination pagination { get; set; }
+}
+
+public class Pagination
+{
+     public int count { get; set; }
+     public string next { get; set; }
+     public Int64 prev { get; set; }
+}
