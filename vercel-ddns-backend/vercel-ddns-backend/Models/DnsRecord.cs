eba3b7e4069c43bc39b50acbdcd8571b11eefa39
Hargaaya <hargaaya10@gmail.com>
2023-01-21 18:42:14 +0100

    convert record to class

--- vercel-ddns-backend/vercel-ddns-backend/Models/DnsRecord.cs
+++ vercel-ddns-backend/vercel-ddns-backend/Models/DnsRecord.cs
@@ -1,18 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿namespace vercel_ddns_backend.Models;
 
-namespace vercel_ddns_backend.Models;
-
-public record DnsRecord(
-    string Id,
-    string Slug,
-    [Required] string Name,
-    [Required] string Type,
-    [Required] string Value,
-    string? MxPriority,
-    string? Priority,
-    string Creator,
-    int? Created,
-    int? Updated,
-    int? CreatedAt,
-    int? UpdatedAt
-    );
+public class DnsRecord
+{
+    public string Id { get; set; }
+    public string Slug { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public string Value { get; set; }
+    public string? MxPriority { get; set; }
+    public string? Priority { get; set; }
+    public string Creator { get; set; }
+    public Int64 Created { get; set; }
+    public Int64 Updated { get; set; }
+    public Int64 CreatedAt { get; set; }
+    public Int64 UpdatedAt { get; set; }
+    public int Ttl { get; set; }
+}
