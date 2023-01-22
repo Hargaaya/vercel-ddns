b6b9e1511e99842cba2a21c0a5f4f6d8d5882ab4
Hargaaya <hargaaya10@gmail.com>
2023-01-22 04:37:59 +0100

    move to "Get" directory

---
+++ vercel-ddns-backend/vercel-ddns-backend/Models/Get/VercelGetResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace vercel_ddns_backend.Models.Get;
+
+public record VercelGetResponse
+{
+    public DnsRecord[]? Records { get; set; }
+    public Pagination? Pagination { get; set; }
+    public Error? Error { get; set; }
+}
+
+public record Pagination
+{
+     public int Count { get; set; }
+     public string Next { get; set; } = string.Empty;
+     public long Prev { get; set; }
+}
