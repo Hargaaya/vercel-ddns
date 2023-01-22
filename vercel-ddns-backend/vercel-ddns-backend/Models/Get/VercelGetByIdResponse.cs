327f90b4b52be69eec234ffab4db1d5077d4e8e1
Hargaaya <hargaaya10@gmail.com>
2023-01-22 04:41:45 +0100

    cleanup

--- vercel-ddns-backend/vercel-ddns-backend/Models/Get/VercelGetByIdResponse.cs
+++ vercel-ddns-backend/vercel-ddns-backend/Models/Get/VercelGetByIdResponse.cs
@@ -2,14 +2,14 @@
 
 public record VercelGetByIdResponse
 {
-    public long CreatedAt { get; set; }
-    public string creator { get; set; }
-    public string domain { get; set; }
-    public string id { get; set; }
-    public string name { get; set; }
-    public int ttl { get; set; }
-    public string value { get; set; }
-    public string recordType { get; set; }
-    public string? type { get; set; }
+    public long? CreatedAt { get; set; }
+    public string? Creator { get; set; }
+    public string? Domain { get; set; }
+    public string? Id { get; set; }
+    public string? Name { get; set; }
+    public int Ttl { get; set; }
+    public string? Value { get; set; }
+    public string? RecordType { get; set; }
+    public string? Type { get; set; }
     public Error? Error { get; set; }
 }
