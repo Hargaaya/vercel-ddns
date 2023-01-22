875a9b5f0639d3d33af7427a20194025e7784125
Hargaaya <hargaaya10@gmail.com>
2023-01-22 04:38:41 +0100

    add method to get by id, create, update and delete records

--- vercel-ddns-backend/vercel-ddns-backend/Interfaces/Services/IDnsService.cs
+++ vercel-ddns-backend/vercel-ddns-backend/Interfaces/Services/IDnsService.cs
@@ -1,11 +1,14 @@
-﻿using vercel_ddns_backend.Models;
+﻿using vercel_ddns_backend.Models.Get;
+using vercel_ddns_backend.Models.Patch;
+using vercel_ddns_backend.Models.Post;
 
 namespace vercel_ddns_backend.Interfaces.Services;
 
 public interface IDnsService
 { 
-    Task<IEnumerable<DnsRecord>?> GetRecords(string domain);
-    Task<DnsRecord?> CreateRecord(string domain, DnsRecord record);
-    Task<DnsRecord?> UpdateRecord(string domain, DnsRecord record);
-    Task<bool> DeleteRecord(string domain, string dnsName);
+    Task<VercelGetResponse?> GetRecords(string domain);
+    Task<VercelGetByIdResponse?> GetRecord(string recordId);
+    Task<VercelPostResponse?> CreateRecord(string domain, VercelPostRequest record);
+    Task<VercelPatchResponse?> UpdateRecord(string recordId, VercelPatchRequest record);
+    Task<bool> DeleteRecord(string domain, string recordId);
 }
--- vercel-ddns-backend/vercel-ddns-backend/Services/VercelDnsService.cs
+++ vercel-ddns-backend/vercel-ddns-backend/Services/VercelDnsService.cs
@@ -1,39 +1,139 @@
-﻿using RestSharp;
+﻿using System.Net;
+using System.Text.Json;
+using RestSharp;
 using vercel_ddns_backend.Interfaces.Services;
-using vercel_ddns_backend.Models;
+using vercel_ddns_backend.Models.Get;
+using vercel_ddns_backend.Models.Patch;
+using vercel_ddns_backend.Models.Post;
 
 namespace vercel_ddns_backend.Services;
 
 public class VercelDnsService : IDnsService
 {
     private const string VercelUrl = "https://api.vercel.com/v4/domains";
-    private readonly RestClient _client;
+    private readonly string? _apiKey;
 
-    public VercelDnsService(IConfiguration config)
+    public VercelDnsService(IConfiguration config) => _apiKey = config.GetValue<string>("DnsApiKey");
+    
+    /// <summary>
+    /// Instansiates a new <c>RestClient</c>.
+    /// </summary>
+    /// <param name="baseUrl">Base Url</param>
+    /// <returns>Instantiated RestClient with API token.</returns>
+    private RestClient GetClient(string baseUrl) =>
+        new RestClient(baseUrl).AddDefaultHeader(KnownHeaders.Authorization, $"Bearer {_apiKey}");
+    
+    /// <summary>
+    /// Gets the 20 latest dns records from Vercel.
+    /// </summary>
+    /// <param name="domain">The domain name.</param>
+    public async Task<VercelGetResponse?> GetRecords(string domain)
     {
-        var apiKey = config.GetValue<string>("DnsApiKey");
-        _client = new RestClient(VercelUrl)
-            .AddDefaultHeader(KnownHeaders.Authorization, $"Bearer {apiKey}");
-    }
-    public async Task<IEnumerable<DnsRecord>?> GetRecords(string domain)
-    {
-        var resp = await _client.GetJsonAsync<VercelGetResponse>($"/{domain}/records");
-        
-        return resp?.records;
+        try
+        {
+            var request = new RestRequest();
+            var resp = await this.GetClient($"{VercelUrl}/{domain}/records")
+                .GetAsync<VercelGetResponse>(request);
+
+            return resp;
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine("Something went wrong getting the records. \n" + exception.Message);
+            return null;
+        }
     }
 
-    public Task<DnsRecord?> CreateRecord(string domain, DnsRecord record)
+    public async Task<VercelGetByIdResponse?> GetRecord(string recordId)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var request = new RestRequest();
+            var resp = await this.GetClient($"{VercelUrl}/records/{recordId}")
+                .GetAsync<VercelGetByIdResponse>(request);
+
+            return resp;
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine("Something went wrong getting the record. \n" + exception.Message);
+            return null;
+        }
     }
 
-    public Task<DnsRecord?> UpdateRecord(string domain, DnsRecord record)
+    /// <summary>
+    /// Creates a dns record on Vercel.
+    /// </summary>
+    /// <param name="domain">The domain name.</param>
+    /// <param name="record">The record data to be added.</param>
+    public async Task<VercelPostResponse?> CreateRecord(string domain, VercelPostRequest record)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var request = new RestRequest().AddJsonBody(record);
+            var resp = await this.GetClient($"{VercelUrl}/{domain}/records")
+                .ExecutePostAsync(request);
+            
+            var serializeOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+            
+            var result = JsonSerializer.Deserialize<VercelPostResponse>(resp.Content!, serializeOptions);
+            
+            return result;
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine("Something went wrong creating a record. \n" + exception.Message);
+            return null;
+        }
     }
 
-    public Task<bool> DeleteRecord(string domain, string dnsName)
+    /// <summary>
+    /// Updates the record on vercel.
+    /// </summary>
+    /// <param name="recordId">The uid for the record.</param>
+    /// <param name="record">The record data to be updated.</param>
+    public async Task<VercelPatchResponse?> UpdateRecord(string recordId, VercelPatchRequest record)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var request = new RestRequest().AddJsonBody(record);
+            var resp = await this.GetClient($"{VercelUrl}/records/{recordId}")
+                .PatchAsync<VercelPatchResponse>(request);
+
+            return resp;
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine("Something went wrong updating a record. \n" + exception.Message);
+            return null;
+        }
     }
+
+    /// <summary>
+    /// Removes a record from Vercel.
+    /// </summary>
+    /// <param name="domain">The domain name.</param>
+    /// <param name="recordId">The uid of the record to remove.</param>
+    /// <returns>True if successful.</returns>
+    public async Task<bool> DeleteRecord(string domain, string recordId)
+    {
+        try
+        {
+            var request = new RestRequest();
+            var resp = await this.GetClient($"{VercelUrl}/{domain}/records/{recordId}")
+                .DeleteAsync(request);
+
+            return resp.StatusCode is HttpStatusCode.OK;
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine("Something went wrong deleting a record. \n" + exception.Message);
+            return false;
+        }
+    }
+
 }
